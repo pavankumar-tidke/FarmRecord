4f3524b8a46f7babeecef4c3835064cce5f824d6
pavankumartidke12345@gmail.com <pavankumartidke12345@gmail.com>
2023-06-10 15:38:25 +0530

    vercel test 8

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,8 @@
 
 echo " BUILD START"
 
-python -m pip3 install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 
 echo " BUILD END"
 
