f3b8b6422e44418714bf539654e78e25cc784d9d
pavankumartidke12345@gmail.com <pavankumartidke12345@gmail.com>
2023-07-03 20:11:43 +0530

    removed module pytz

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 
 
 
-echo " BUILD START"
+echo "------------------------------------------------- BUILD START -------------------------------------------------"
 
 python3.9 -m pip install psycopg2-binary
 python3.9 -m pip install pymongo==3.12.3
@@ -9,6 +9,6 @@ python3.9 -m pip install djongo --upgrade
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 
-echo " BUILD END"
+echo "-------------------------------------------------- BUILD END --------------------------------------------------"
 
 
