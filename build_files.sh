76a2ff893496d9ef68aa1d8da2e1e693a77f659d
pavankumartidke12345@gmail.com <pavankumartidke12345@gmail.com>
2023-07-05 15:08:14 +0530

    step 2: changes the djongo version to 1.3.3

--- build_files.sh
+++ build_files.sh
@@ -5,7 +5,7 @@ echo "------------------------------------------------- BUILD START ------------
 
 python3.9 -m pip install psycopg2-binary
 python3.9 -m pip install pymongo==3.12.3
-python3.9 -m pip install djongo --upgrade
+python3.9 -m pip install djongo==1.3.3
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
 
