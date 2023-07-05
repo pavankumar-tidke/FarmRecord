97dbf30a4b51625d8b105e6fcae6cfd6e42ebcfe
pavankumartidke12345@gmail.com <pavankumartidke12345@gmail.com>
2023-07-05 14:40:50 +0530

    changes to the build_sh

--- build_files.sh
+++ build_files.sh
@@ -7,7 +7,7 @@ python3.9 -m pip install psycopg2-binary
 python3.9 -m pip install pymongo==3.12.3
 python3.9 -m pip install djongo --upgrade
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput
 
 echo "-------------------------------------------------- BUILD END --------------------------------------------------"
 
