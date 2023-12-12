276c1b75a8bc23680f7dd71f89d2a2ee385a4d07
pavankumar-tidke <pavankumartidke12345@gmail.com>
2023-12-12 15:01:45 +0530

    remove NaN from dashboard

--- src/pages/user/Dashboard/Index.js
+++ src/pages/user/Dashboard/Index.js
@@ -16,6 +16,7 @@ const Dashboard = () => {
       setPageLoading(true);
 
       try {
+        console.log(`${process.env.REACT_APP_BACKEND_URL}/fetch/all_work/`);
         const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/fetch/all_work/`);
         let response = JSON.parse(res.data);
         if (response.success) {
@@ -60,13 +61,11 @@ const Dashboard = () => {
     
     setTotalSoyaAmount((soyabinTotal === 0 || soyabinTotal === undefined) ? 0 : formatNumber(soyabinTotal));
     setTotalChanaAmount((chanaTotal === 0 || chanaTotal === undefined) ? 0 : formatNumber(chanaTotal));
-    let total = soyabinTotal + chanaTotal;
-    setTotalCropAmount((total === 0 || total === undefined) ? 0 : formatNumber(total));
-    // setTotalCropAmount(total)
 
-    // console.log((soyabinTotal === 0) ? 0 : formatNumber(soyabinTotal));
-    // console.log((chanaTotal === 0) ? 0 : formatNumber(chanaTotal));
-    console.log();
+    let total = soyabinTotal + chanaTotal;
+    console.log(typeof(total));
+
+    setTotalCropAmount((isNaN(total) || total === undefined) ? 0 : formatNumber(total));
 
   }, [data]);
 
@@ -87,25 +86,24 @@ const Dashboard = () => {
       <div className="mx-3 space-y-4 py-5 rounded-2xl backdrop-blur-2xl bg-white bg-opacity-5">
 
         <div className="text-center mx-auto mb-8">
-          <h5 className="text-base font-bold text-gray-600 dark:text-gray-300">Total Revenue</h5>
+          <h5 className="text-base font-bold text-gray-600 dark:text-gray-300">Total Revenue <span className=" text-xs text-gray-500">(since 2020)</span></h5>
           <h5 className="text-3xl font-bold text-green-600">
             <span className="material-symbols-outlined extra-small-icon text-slate-500 dark:text-slate-500">currency_rupee</span>
-            {totalCropAmount}
-            <span className="ml-5 text-xs text-gray-500">(since 2020)</span>
+            {totalCropAmount} 
           </h5>
         </div>
   
         <div className="flex justify-evenly text-center mb-6">
           <div className="">
             <h5 className="text-base font-bold text-gray-600 dark:text-gray-300">Chana Profit</h5>
-            <h5 className="text-lg font-bold text-red-400">
+            <h5 className="text-lg font-bold text-blue-400">
               <span className="material-symbols-outlined extra-small-icon text-slate-500 dark:text-slate-500">currency_rupee</span>
               {totalChanaAmount}
             </h5>
           </div>
           <div className="">
             <h5 className="text-base font-bold text-gray-600 dark:text-gray-300">Soyabin Profit</h5>
-            <h5 className="text-lg font-bold text-green-600">
+            <h5 className="text-lg font-bold text-blue-400">
               <span className="material-symbols-outlined extra-small-icon text-slate-500 dark:text-slate-500">currency_rupee</span>
               {totalSoyaAmount}
             </h5>
